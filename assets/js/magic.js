5705ccb5a4412a34c56b4030e78527d5b880be08
Asael Chávez <asaelx@gmail.com>
2015-10-26 06:55:16 -0600

    Scripts, modal and videos

---
+++ assets/js/magic.js
@@ -0,0 +1,88 @@
+$(function(){
+
+    var body = $('body')
+
+    $.fn.visible = function(partial) {
+
+        var $t            = $(this),
+        $w            = $(window),
+        viewTop       = $w.scrollTop(),
+        viewBottom    = viewTop + $w.height(),
+        _top          = $t.offset().top,
+        _bottom       = _top + $t.height(),
+        compareTop    = partial === true ? _bottom : _top,
+        compareBottom = partial === true ? _top : _bottom;
+
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+    };
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            // $(document).on("scroll", onScroll);
+        });
+    });
+
+    var layer = $('#layer'),
+        modal = $('#modal'),
+        video = modal.find("video")[0];
+
+    function showModal(){
+        layer.removeClass('close').addClass('open');
+        modal.show();
+        video.play();
+    }
+
+    function closeModal(){
+        video.pause();
+        layer.removeClass('open').addClass('close');
+    }
+
+    $('.play-btn').on('click', function(){
+        showModal();
+    });
+
+    body.on('click', '#layer', function(){
+        closeModal();
+    });
+
+    body.on('click', '#close', function(){
+        closeModal();
+    });
+
+    $(document).keyup(function(e){
+        if(e.keyCode == 27){
+            closeModal();
+        }
+    });
+
+    function onScroll(event){
+
+        var sections = $('.content');
+
+        sections.each(function(i, el){
+            var el = $(el);
+            if(el.visible(true)){
+                el.addClass('reveal');
+            }
+        });
+    }
+
+    $(document).on('scroll', onScroll);
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
 
 gulp.task('templates', function(){
-    gulp.src('assets/jade/*.jade')
+    return gulp.src('assets/jade/*.jade')
         .pipe(jade())
         .pipe(gulp.dest('public/'));
 });
 
 gulp.task('styles', function(){
-    gulp.src('assets/sass/styles.sass')
+    return gulp.src('assets/sass/styles.sass')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('public/css/'))
+});
+
+gulp.task('scripts', function(){
+    return gulp.src(['assets/js/jquery.min.js', 'assets/js/plugins/*.js', 'assets/js/*.js'])
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js/min'));
 });
 
 gulp.task('default', function(){
     gulp.watch('assets/jade/*.jade', ['templates']);
     gulp.watch('assets/sass/*.sass', ['styles']);
+    gulp.watch('assets/js/*.js', ['scripts']);
 });
