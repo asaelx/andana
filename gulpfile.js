425fd83583340cbc1dc99a1ad2bb692fa109b261
Asael Chávez <asaelx@gmail.com>
2015-10-26 04:25:04 -0600

    Some styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    jade = require('gulp-jade');
+    jade = require('gulp-jade'),
+    sass = require('gulp-sass');
 
 gulp.task('templates', function(){
     gulp.src('assets/jade/*.jade')
@@ -7,6 +8,13 @@ gulp.task('templates', function(){
         .pipe(gulp.dest('public/'));
 });
 
+gulp.task('styles', function(){
+    gulp.src('assets/sass/styles.sass')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('public/css/'))
+});
+
 gulp.task('default', function(){
     gulp.watch('assets/jade/*.jade', ['templates']);
+    gulp.watch('assets/sass/*.sass', ['styles']);
 });
